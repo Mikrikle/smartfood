3144f10c7eec4d86d3b89f5752e9352d6ae6344b
Anton Kurashkevich <mr.kur.anton@gmail.com>
2021-12-12 17:28:36 +0300

    created faq page

--- siteapp/urls.py
+++ siteapp/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     # /siteapp/
     path('', views.IndexView.as_view(), name='index'),
+    path('faq/', views., name='faq'),
     path('u/', views.UserInfoView.as_view(), name='info'),
     path('customvision/', views.BackgroundImageAnalyze.as_view(), name="customvision"),
     path('foodanalize/', views.BackgroundFoodAnalzeView.as_view(), name="foodanalize")
